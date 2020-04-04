4de803e7411eddd228a10533dd1f90febac564f1
serii <seriiburduja@gmail.com>
2020-04-04 12:09:01 +0300

    big-menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,21 +1,14 @@
 $(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-	$("html").easeScroll({
-		frameRate: 60,
-		animationTime: 1000,
-		stepSize: 90,
-		pulseAlgorithm: 1,
-		pulseScale: 8,
-		pulseNormalize: 1,
-		accelerationDelta: 20,
-		accelerationMax: 1,
-		keyboardSupport: true,
-		arrowScroll: 50,
-		touchpadSupport: true,
-		fixedBackground: true
-	});
+
+	let closeMenu = function () {
+		$('#js-sandwitch-wrap').on('click', function () {
+			$('body').toggleClass('fixed');
+			$('#js-main-menu').slideToggle();
+
+			$(this).toggleClass('sandwitch--active');
+		});
+	};
+	closeMenu();
 
 	//btn arrow
 	let btnArrowFunc = function () {
@@ -36,7 +29,7 @@ $(function () {
 			let carouselId = "carousel" + idx;
 			this.closest('.cards__item').id = carouselId;
 
-			if($(this).length > 0){
+			if ($(this).length > 0) {
 				$(this).slick({
 					arrows: false,
 					dots: true,
@@ -72,27 +65,27 @@ $(function () {
 		$('#js-fullscreen-menu .fullscreen-menu__item').on('mouseenter', function () {
 			let dataFullscreenMenu = $(this).attr('data-fullscreen-menu');
 
-			if(fullscreenMenu.hasClass('show')){
+			if (fullscreenMenu.hasClass('show')) {
 				console.log('Has class show');
 				fullscreenMenu.removeClass('show');
 				fullscreenMenu.css({
 					'background-image': 'url(assets/i/fullscreen-menu/' + dataFullscreenMenu + ')'
 				});
 
-				setTimeout(function(){
+				setTimeout(function () {
 					fullscreenMenu.addClass('show');
-				},200);
-			}else{
+				}, 200);
+			} else {
 				console.log('Has class show');
 			}
 
 		});
 	};
 
-	let keysSounds = function(){
+	let keysSounds = function () {
 		$('#js-keys').on('keydown', function (e) {
-			let key = $(this).find('.key[data-key='+e.which+']');
-			let audio = $(this).find('audio[data-key='+e.which+']')[0];
+			let key = $(this).find('.key[data-key=' + e.which + ']');
+			let audio = $(this).find('audio[data-key=' + e.which + ']')[0];
 
 
 			$('#js-keys .key').removeClass('playing');
